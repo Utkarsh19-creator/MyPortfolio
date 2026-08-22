dde581798e509525e46baaf00645ef4786daf88d
Utkarsh19-creator <https://github.com/Utkarsh19-creator.com>
2026-08-22 19:01:10 +0530

    fixed

--- utkarshdev-main/vite.config.ts
+++ utkarshdev-main/vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), tailwindcss(), react(), tsconfigPaths()],
+  plugins: [tanstackStart(), tailwindcss(), react(), tsconfigPaths(), nitro()],
 });
